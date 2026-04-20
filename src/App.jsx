e5c7ef613c06b06041a5a57fd48662acb06d9b56
ElioSpena <elio.spena.94@gmail.com>
2026-04-20 17:28:25 +0200

    feat: creato hook useDate

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import useSwitch from "../hooks/useSwitch";
+import useDate from "../hooks/useDate";
+import { useEffect } from "react";
 
 function App() {
   const [isOn, toggle] = useSwitch();
+  const currentDate = useDate();
 
   return (
-    <div>
-      <h1>Il valore è: {isOn ? "ON" : "OFF"}</h1>
-      <button onClick={toggle}>Cambia Stato</button>
-    </div>
+    <>
+      {/*useSwitch */}
+      <div>
+        <h1>Il valore è: {isOn ? "ON" : "OFF"}</h1>
+        <button onClick={toggle}>Cambia Stato</button>
+      </div>
+
+      {/*useDate*/}
+      <div>
+        <h1>Data e ora attuali:</h1>
+        <p>{currentDate.toLocaleString()}</p>
+      </div>
+    </>
   );
 }
 
